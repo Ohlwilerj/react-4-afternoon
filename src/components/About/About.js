7b7d15807c934be8e525902ee5dc3ee0c8a40e15
Jordan Ohlwiler <ohlwilerj@gmail.com>
2019-09-10 16:47:28 -0600

    routing done + black diamond

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
+import History from '../History/History'
+import Contact from '../Contact/Contact'
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div className='subnav'>
-        </div>
+          <div className='subnav'>
+            <Link to='/about' className='subnav_links'><h3>About</h3></Link>
+            <Link to='/about/history' className='subnav_links'><h3>History</h3></Link>
+            <Link to='/about/contact' className='subnav_links'><h3>Contact</h3></Link>
+          </div>
         <div className='box'>
-        </div>
+          <Switch>
+            <Route path='/about/history' component={History} />
+            <Route path='/about/Contact' component={Contact} />
+            <Route path='/about' render={() => (
+              <div>
+                <h1>About the University</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus possimus architecto dolorem velit placeat necessitatibus asperiores dolorum rem suscipit adipisci atque ullam nostrum eum reiciendis, dignissimos totam aliquam recusandae eveniet.</p>
+              </div>
+            )}
+            />
+          </Switch>
+          </div>
       </div>
     )
   }
